e8649a34db96ab90c043571416fbe40a691a7053
Kawser Ahamed <71338839+Kawserahamed@users.noreply.github.com>
2021-06-08 07:48:40 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,54 @@
+
+   // sticky top menu 
+   $(window).scroll(function(){
+
+    var scrolling = $(this).scrollTop();
+    var stickyMenu = $('.navbar-light');
+
+    if (scrolling >=100) {
+
+      stickyMenu.addClass('nav-bg') ;  
+
+    } else {
+      stickyMenu.removeClass('nav-bg') ;
+      
+    }
+  })
+
+
 AOS.init();
 
-$('.popup-link').magnificPopup({
-type: 'image'
-// other options
-});
 
 
 $('.counter').counterUp({
   delay: 10,
   time: 1000
+});
+
+
+$('.image').magnificPopup({
+  type:'image',
+  gallery:{
+    enabled:true
+  }
+});
+
+
+
+// slick
+
+$('.project-slick').slick({
+  autoplaySpeed:1500,
+  lazyLoad: 'ondemand',
+  slidesToShow: 3,
+  slidesToScroll: 1
+  });
+
+
+$('.blog-slick').slick({
+autoplaySpeed:2000,
+lazyLoad: 'ondemand',
+slidesToShow: 2,
+slidesToScroll: 1
 });
 
